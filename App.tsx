d5bef7e054ad99cb381976a3b7d00bac1c3e67b2
thakkarnetram <thakkarnetram10@gmail.com>
2025-02-04 15:23:40 +0530

    completed home ui and added sqlite need to add queries

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './src/components/navigation/RootStack';
+import SQLite from 'react-native-sqlite-storage';
+
+const db = SQLite.openDatabase(
+  {
+    name: 'little-lemon.db',
+    location: 'default'
+  },
+  () => console.log('Database open'),
+  error => console.error('Could not open database'),
+);
 
 function App(): JSX.Element {
+
   return (
     <NavigationContainer>
       <RootStack />
--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -1,37 +1,173 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, Image, StyleSheet, Animated, TouchableOpacity} from 'react-native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../navigation/RootStack';
+import FlatList = Animated.FlatList;
 
 type HomeNavProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
 
 type Props = {
   navigation: HomeNavProp;
 };
-const Home: React.FC<Props> = ({navigation}) => {
+const Home: React.FC<Props> = () => {
+  const [data, setData] = useState([]);
+  const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch(
+          'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json'
+        );
+        const response = await res.json();
+        if (res.ok) {
+          setData(response.menu);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    getData();
+  }, []);
+
+  const filters = ['Starters', 'Mains', 'Drinks' ,'Desserts'];
+
   return (
     <View style={{height: '100%', backgroundColor: '#f4e7d2'}}>
+      <View style={styles.headerContainer}>
+        <View style={styles.textContainer}>
+          <Text style={styles.headerText}>Little Lemon Resto</Text>
+          <Text style={styles.desc}>Family owned restaurant {'\n'}Traditional dishes with a modern twist</Text>
+        </View>
+        <Image
+          source={{uri: 'https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/greekSalad.jpg?raw=true'}}
+          style={styles.imageHome}
+        />
+      </View>
 
+      <View style={styles.filterContainer}>
+        <Text style={styles.filterHead}>Order Now</Text>
+        <View style={styles.filterButtons}>
+          {filters.map(filter => (
+            <TouchableOpacity
+              key={filter}
+              style={[styles.filterButton, selectedFilter === filter && styles.activeFilter]}
+              onPress={() => setSelectedFilter(filter)}
+            >
+              <Text style={[styles.filterText, selectedFilter === filter && styles.activeFilterText]}>{filter}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View style={styles.foodContainer}>
+            <View style={{ flex: 1 }}>
+              <Text style={styles.foodHeader}>{item.name}</Text>
+              <Text style={styles.foodDescription} numberOfLines={2}>{item.description}</Text>
+            </View>
+            <Image
+              source={{
+                uri: `https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/${item.image}?raw=true`,
+              }}
+              style={styles.foodImage}
+            />
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  logoImage: {
-    width: 400,
-    height: 60,
-    marginTop: '10%',
-    resizeMode: 'contain',
+  headerContainer: {
+    backgroundColor: '#003b18',
+    width: '100%',
+    height: '25%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15,
   },
-  image: {
-    width: '80%',
-    height: '40%',
-    margin: '10%',
+  textContainer: {
+    flex: 1,
   },
-  profileLink: {
-    fontSize: 24,
-    textAlign: 'center',
+  headerText: {
+    color: '#fcd200',
+    fontSize: 26,
+  },
+  desc: {
+    fontSize: 18,
+    color: '#ffffff',
+    marginTop: 10,
+  },
+  imageHome: {
+    width: 150,
+    height: 100,
+    borderRadius: 15,
+  },
+  filterContainer: {
+    width: '100%',
+    height: '15%',
+    backgroundColor: '#fffdf5',
+    padding: 15,
+  },
+  filterHead: {
     color: 'black',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop:5,
+  },
+  filterButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 10,
+  },
+  filterButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    backgroundColor: '#dcdcdc',
+    marginTop: 10,
+  },
+  activeFilter: {
+    backgroundColor: '#003b18',
+  },
+  filterText: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  activeFilterText: {
+    color: 'white',
+  },
+  foodContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    padding:35,
+    borderBottomWidth: 1,
+    borderBottomColor: '#E5E5E5',
+  },
+  foodHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  foodDescription: {
+    fontSize: 12,
+    color: '#4F4F4F',
+    marginTop: 5,
+    flexShrink: 1,
+  },
+  foodImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+    marginLeft: 10,
   },
 });
 
